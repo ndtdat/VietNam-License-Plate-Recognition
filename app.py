f43bc1e561b0bd4b810f9f98a5bdc3ca8a7d291e
Dat Nguyen <tiendat3550@gmail.com>
2020-07-24 01:03:51 +0700

    commit

--- app.py
+++ app.py
@@ -21,25 +21,25 @@ def _hello_world():
 	return "Hello world"
 
 # Khai báo các route 2 cho API
-@app.route("/predict", methods=["POST", "GET"])
-# Khai báo hàm xử lý dữ liệu.
-def _predict():
-	data = {"success": False}
-	if request.files.get("image"):
-		# Lấy file ảnh người dùng upload lên
-		image = request.files["image"].read()
-		# Convert sang dạng array image
-		image_stream = io.BytesIO(image)
-		file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
-		img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-		# recognize license plate
-		lpNumber = model.predict(img)
-		data["license_plate"] = lpNumber
-		data["success"] = True
-	return json.dumps(data, ensure_ascii=False, cls=utils.NumpyEncoder)
+# @app.route("/predict", methods=["POST", "GET"])
+# # Khai báo hàm xử lý dữ liệu.
+# def _predict():
+# 	data = {"success": False}
+# 	if request.files.get("image"):
+# 		# Lấy file ảnh người dùng upload lên
+# 		image = request.files["image"].read()
+# 		# Convert sang dạng array image
+# 		image_stream = io.BytesIO(image)
+# 		file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+# 		img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+# 		# recognize license plate
+# 		lpNumber = model.predict(img)
+# 		data["license_plate"] = lpNumber
+# 		data["success"] = True
+# 	return json.dumps(data, ensure_ascii=False, cls=utils.NumpyEncoder)
 
 if __name__ == "__main__":
 	print("App run!")
 	# Load model
-	model = E2E()
+	# model = E2E()
 	app.run()
